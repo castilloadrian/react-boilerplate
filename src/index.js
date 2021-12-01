c8933eb4a22602f5e09956098e67d18afde183ce
Adrian Castillo <adriancastillo@Adrians-MBP.home>
2021-11-30 20:39:04 -0600

    Functioning react boiler-plate

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/homepage.css';
+import App from './js/homepage';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/js/homepage.js
@@ -0,0 +1,12 @@
+function App() {
+    return (
+      <html>
+        <div className="App">
+          <h1>House of Cards and Collectibles! 🎆</h1>
+        </div>
+      </html>
+    );
+  }
+  
+export default App;
+  
